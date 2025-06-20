1b54232a4ad3ac490bc27077eacfacd97d688693
Adrian Lopez <adrianlzt@gmail.com>
2025-06-20 20:09:45 +0200

    feat: jszip to export project

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
+    allowedHosts: [
+      '.ts.net',
+    ],
+  },
+
 })
 
